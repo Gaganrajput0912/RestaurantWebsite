afe003c150ebd297f6d3c5b9b0c01b2cae4c6585
Gaganrajput0912 <Gaganrajput717114@gmail.com>
2023-02-14 21:02:43 +0530

    first commit

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -1,16 +1,25 @@
 import Card from './components/UI/Card';
 import './App.css';
 import Cart from './components/Cart/Cart';
-import React from 'react';
+import React, {useState} from 'react';
 import Meals from './components/Meals/Meals';
 import Header from './components/Layout/Header';
+
 function App() {
+  const[cartIsShown,setCartIsShown]=useState(false)
+  const showCartHandler = ()=>{
+    setCartIsShown(true)
+  }
+
+  const hideCartHandler = ()=>{
+    setCartIsShown(false)
+  }
   return (
    <div>
     <Card>
       <React.Fragment>
-        <Cart />
-      <Header></Header>
+        {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler} ></Header>
       <main>
 <Meals></Meals>
       </main>
--- restaurant/src/components/UI/Modal.js
+++ restaurant/src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import React from 'react'
 import  ReactDOM  from 'react-dom'
 const Backdrop = props =>{
 
-return <div className={classes.backdrop}></div>
+return <div className={classes.backdrop} onClick={props.onClose}></div>
 }
 
 const ModalOverlay  = props =>{
@@ -17,7 +17,7 @@ const Modal  = props =>{
 
     return (
         <React.Fragment>
-            {ReactDOM.createPortal(<Backdrop />,portal)}
+            {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>,portal)}
             {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>,portal)}
             
         </React.Fragment>
