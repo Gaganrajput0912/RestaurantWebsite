11eb0b42b8e7bd1193082fe9835979730c2327f0
Gaganrajput0912 <Gaganrajput717114@gmail.com>
2023-02-14 12:06:05 +0530

    first commit

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -1,9 +1,16 @@
-
+import Card from './components/UI/Card';
 import './App.css';
-
+import React from 'react';
+import Header from './components/Layout/Header';
 function App() {
   return (
-    <div></div>
+   <div>
+    <Card>
+      <React.Fragment>
+      <Header></Header>
+      </React.Fragment>
+    </Card>
+   </div>
   );
 }
 
