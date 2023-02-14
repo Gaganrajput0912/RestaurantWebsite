f7e062a735ebdaee874662f48c7c0c8dada64d6d
Gaganrajput0912 <Gaganrajput717114@gmail.com>
2023-02-14 12:25:58 +0530

    first commit

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -1,6 +1,7 @@
 import Card from './components/UI/Card';
 import './App.css';
 import React from 'react';
+import Meals from './components/Meals/Meals';
 import Header from './components/Layout/Header';
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
     <Card>
       <React.Fragment>
       <Header></Header>
+      <main>
+<Meals></Meals>
+      </main>
       </React.Fragment>
     </Card>
    </div>
