123be96e1e4d2676b8d1e6a1ea8a9302a75e4fd9
Gaganrajput0912 <Gaganrajput717114@gmail.com>
2023-02-14 19:45:27 +0530

    first commit

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -1,5 +1,6 @@
 import Card from './components/UI/Card';
 import './App.css';
+import Cart from './components/Cart/Cart';
 import React from 'react';
 import Meals from './components/Meals/Meals';
 import Header from './components/Layout/Header';
@@ -8,6 +9,7 @@ function App() {
    <div>
     <Card>
       <React.Fragment>
+        <Cart />
       <Header></Header>
       <main>
 <Meals></Meals>
--- restaurant/src/components/Meals/AvailableMeals.js
+++ restaurant/src/components/Meals/AvailableMeals.js
@@ -29,7 +29,7 @@ const DUMMY_MEALS = [
     },
   ];
 const AvailableMeals = ()=>{
-    const mealsList = DUMMY_MEALS.map(meal=><MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}></MealItem>)
+    const mealsList = DUMMY_MEALS.map(meal=><MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price}></MealItem>)
     return(
         <React.Fragment>
 <section className={classes.meal}>
